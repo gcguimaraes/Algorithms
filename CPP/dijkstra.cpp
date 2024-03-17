f77269466ad7348828c2c8438623d2b04cbe4e84
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-03-17 20:34:39 -0300

    Create dijkstra.cpp
    NOT SURE IT WORKS

---
+++ CPP/dijkstra.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 1e9
+#define ii pair<int, int>
+
+vector<vector<ii>> graph;
+
+vector<int> dijkstra(int s, int n) {
+    vector<int> d(n, INF);
+    d[s] = 0;
+    
+    priority_queue<ii, vector<ii>, greater<ii>> pq;
+    pq.push({0, s});
+    
+    while (!pq.empty()) {
+        ii dv = pq.top(); pq.pop();
+       
+        int dist = dv.first;
+        int v = dv.second;
+        
+        if (dist != d[v])
+            continue;
+            
+        for (dv : graph[v]) {
+            int newDist = dist + dv.first;
+            int next = dv.second;
+            if (newDist < d[next]) {
+                d[next] = newDist;
+                pq.push({newDist, next});
+            }
+        }
+    }
+    return d;
+}
