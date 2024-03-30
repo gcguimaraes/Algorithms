27edae352371a4ce94042ce81017aad55ad5c25f
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-03-30 18:46:51 -0300

    Now it detaches itself from C

--- CPP/template.cpp
+++ CPP/template.cpp
@@ -4,15 +4,11 @@ using namespace std;
 
 #define INF 1000000000
 #define ii pair<int, int>
+#define detachingFromC ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endl "\n"
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-  
-
-  
-
+    detachingFromC;
+    
     return 0;
 }
