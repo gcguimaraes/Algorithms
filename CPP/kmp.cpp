eabf5be5dadc2727ef8fc6917320fadaeeaa3201
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-07-26 18:17:20 -0300

    Update kmp.cpp

--- CPP/kmp.cpp
+++ CPP/kmp.cpp
@@ -14,22 +14,13 @@ vector<int> buildLps(string &s) {
 
 int main() {
     // every time a pattern matches, the lcs value of that position will be 'n' (the pat size)
-    // example:
-    // aaaa#zzaaaa
-    // lps[i=10] = 4 (pat size)
-    // if you subtract (pat size - 1), you will get the index the pattern starts on the merged string
-    // but since you want the index on the original string, you still gotta subtract (pat + 1)
-    // (because of the pattern and '#' from the start)
-    // simplifying, the index is (i - 2*n)
-
+    
     string text, pat;
     string patThenText = pat + '#' + text; // '#' should not appear on any of the strings
     vector<int> lps = buildLps(patThenText);
     int n = pat.length();
     int m = text.length();
     vector<int> indices;
-    // n+1 points to the char next to the '#'
-    // n + m + 1 is the size of the merged string
     for (int i = n+1; i < n+m+1; i++)
         if (lps[i] == n)
             indices.push_back(i - 2*n);
