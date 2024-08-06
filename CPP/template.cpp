f047440ef61ced58c26299013adaea8d2e525015
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-08-06 11:22:52 -0300

    Update template.cpp

--- CPP/template.cpp
+++ CPP/template.cpp
@@ -7,7 +7,7 @@ using ii = pair<int, int>;
 using ll = long long;
 #define detachingFromC ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 
-int main() {
+signed main() {
     detachingFromC;
     
     return 0;
