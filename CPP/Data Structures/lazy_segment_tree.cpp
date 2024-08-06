6fdab4db240c784c059d9f28ba992b2b0c9eb968
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-08-06 17:26:19 -0300

    Update lazy_segment_tree.cpp

--- CPP/Data Structures/lazy_segment_tree.cpp
+++ CPP/Data Structures/lazy_segment_tree.cpp
@@ -1,100 +1,94 @@
 class LazySegmentTree {
-    struct Node {
-        int min_val;
-        int count;
-        int lazy;
+    struct Segment {
+        int min, count, l, r, lazy;
     };
 
-    std::vector<Node> tree;
-    int n;
+    ii NEUTRAL = {INF, 1};
+    static const int MAXN = 200010;
+    Segment t[4*MAXN];
 
-    void buildTree(int v, int tl, int tr) {
-        if (tl == tr) {
-            tree[v] = {0, 1, 0};
-        } else {
-            int tm = (tl + tr) / 2;
-            buildTree(v*2, tl, tm);
-            buildTree(v*2+1, tm+1, tr);
+    void build(vector<int> &a, int v, int tl, int tr) {
+        t[v] = {INF, 1, tl, tr, 0};
+        if (tl == tr)
+            t[v].min = a[tl];
+        else {
+            int mid = (tl + tr) / 2;
+            build(a, 2*v, tl, mid);
+            build(a, 2*v+1, mid+1, tr);
             merge(v);
-        }
-    }
-
-    void apply(int v, int tl, int tr, int val) {
-        tree[v].min_val += val;
-        tree[v].lazy += val;
-    }
-
-    void push(int v, int tl, int tr) {
-        if (tree[v].lazy != 0) {
-            int tm = (tl + tr) / 2;
-            apply(v*2, tl, tm, tree[v].lazy);
-            apply(v*2+1, tm+1, tr, tree[v].lazy);
-            tree[v].lazy = 0;
         }
     }
 
     void merge(int v) {
-        if (tree[v*2].min_val < tree[v*2+1].min_val) {
-            tree[v].min_val = tree[v*2].min_val;
-            tree[v].count = tree[v*2].count;
-        } else if (tree[v*2].min_val > tree[v*2+1].min_val) {
-            tree[v].min_val = tree[v*2+1].min_val;
-            tree[v].count = tree[v*2+1].count;
-        } else {
-            tree[v].min_val = tree[v*2].min_val;
-            tree[v].count = tree[v*2].count + tree[v*2+1].count;
+        if (t[2*v].min < t[2*v+1].min) {
+            t[v].min = t[2*v].min;
+            t[v].count = t[2*v].count;
+        }
+        else if (t[2*v].min > t[2*v+1].min) {
+            t[v].min = t[2*v+1].min;
+            t[v].count = t[2*v+1].count;
+        }
+        else {
+            t[v].min = t[2*v].min;
+            t[v].count = t[2*v].count + t[2*v+1].count;
         }
     }
 
-    void updateRange(int v, int tl, int tr, int l, int r, int addend) {
-        if (l > r) return;
-        if (l == tl && r == tr) {
-            apply(v, tl, tr, addend);
-        } else {
-            push(v, tl, tr);
-            int tm = (tl + tr) / 2;
-            updateRange(v*2, tl, tm, l, std::min(r, tm), addend);
-            updateRange(v*2+1, tm+1, tr, std::max(l, tm+1), r, addend);
-            merge(v);
+    ii merge(ii &a, ii &b) {
+        if (a.first < b.first)
+            return {a.first, a.second};
+        else if (a.first > b.first)
+            return {b.first, b.second};
+        return {a.first, a.second + b.second};
+    }
+
+    void push(int v) {
+        if (t[v].lazy) {
+            t[v].min += t[v].lazy;
+            if (t[v].l != t[v].r) { // not leaf
+                t[2*v].lazy += t[v].lazy;
+                t[2*v+1].lazy += t[v].lazy;
+            }
+            t[v].lazy = 0;
         }
     }
 
-    std::pair<int, int> queryRange(int v, int tl, int tr, int l, int r) {
-        if (l > r) return {INT_MAX, 0};
-        if (l <= tl && tr <= r) {
-            return {tree[v].min_val, tree[v].count};
+    ii query(int v, int l, int r) {
+        push(v);
+        if (t[v].l > r || t[v].r < l)
+            return NEUTRAL;
+        if (t[v].l >= l && t[v].r <= r)
+            return {t[v].min, t[v].count};
+        
+        ii left = query(2*v, l, r);
+        ii right = query(2*v+1, l, r);
+        return merge(left, right);
+    }
+
+    void update(int v, int l, int r, int delta) {
+        push(v);
+        if (t[v].l > r || t[v].r < l)
+            return;
+        if (t[v].l >= l && t[v].r <= r) {
+            t[v].lazy += delta;
+            push(v);
+            return;
         }
-        push(v, tl, tr);
-        int tm = (tl + tr) / 2;
-        auto left = queryRange(v*2, tl, tm, l, std::min(r, tm));
-        auto right = queryRange(v*2+1, tm+1, tr, std::max(l, tm+1), r);
-        if (left.first < right.first) {
-            return left;
-        } else if (left.first > right.first) {
-            return right;
-        } else {
-            return {left.first, left.second + right.second};
-        }
+        update(2*v, l, r, delta);
+        update(2*v+1, l, r, delta);
+        merge(v);
     }
 
 public:
-    LazySegmentTree(int _n) {
-    	n = _n;
-        tree.resize(4*n);
-        buildTree(1, 0, n-1);
+    LazySegmentTree(vector<int> &a) {
+        build(a, 1, 0, a.size()-1);
     }
 
-    void updateRange(int l, int r, int addend) {
-        updateRange(1, 0, n-1, l, r, addend);
+    ii query(int l, int r) {
+        return query(1, l, r);
     }
 
-    std::pair<int, int> queryRange(int l, int r) {
-        return queryRange(1, 0, n-1, l, r);
+    void update(int l, int r, int delta) {
+        update(1, l, r, delta);
     }
-    
-    int get_maintained(){
-    	pair<int, int> res = queryRange(0, n-1);
-    	assert(res.first == 0);
-    	return n - res.second;
-    };
 };
