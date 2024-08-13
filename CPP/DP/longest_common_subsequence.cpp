acb45d84f54b9ee6b80a7e6cc938a69c16e2014a
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-08-12 21:46:41 -0300

    Update longest_common_subsequence.cpp

--- CPP/DP/longest_common_subsequence.cpp
+++ CPP/DP/longest_common_subsequence.cpp
@@ -9,3 +9,22 @@ int lcs(string &a, string &b, int i, int j) {
         lcs(a, b, i-1, j)
     );
 }
+
+string lcsRecover() {
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    lcs(i, j);
+    string res = "";
+
+    while (i >= 0 && j >= 0) {
+        if (a[i] == b[j]) {
+            res += a[i];
+            i--; j--;
+        }
+        else if (i > 0 && dp[i][j] == dp[i-1][j]) i--;
+        else j--;
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
