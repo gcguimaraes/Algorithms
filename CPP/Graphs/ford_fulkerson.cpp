93eeaa6a9cbb688f9c0c135003c6a932beb437f6
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-08-22 13:28:16 -0300

    Update ford_fulkerson.cpp

--- CPP/Graphs/ford_fulkerson.cpp
+++ CPP/Graphs/ford_fulkerson.cpp
@@ -1,43 +1,46 @@
-const int INF = 1e9;
-
-vector<int> p; // parent
-vector<bool> visited;
-vector<vector<int>> graph;
-const int N = 1000;
-int capacity[N+1][N+1];
-
-bool dfs(int u, int t) {
-    visited[u] = true;
-    
-    if (u == t)
-        return true;
-    
-    for (int next : graph[u]) {
-        if (!visited[next] && capacity[u][next]) {
-            if (dfs(next, t)) {
-                p[next] = u;
-                return true;
+class FordFulkerson {
+    bool __dfs(int u, int t) {
+        visited[u] = 1;
+        if (u == t) return true;
+        for (int next : adj[u]) {
+            if (!visited[next] && cap[u][next]) {
+                if (__dfs(next, t)) {
+                    p[next] = u;
+                    return true;
+                }
             }
         }
+        return false;
     }
-    
-    return false;
-}
 
-int fordFulkerson(int s, int t) {
-    int maxflow = 0;
-    while (dfs(s, t)) {
-        int increment = INF;
-        for (int u = t; p[u] >= 0; u = p[u]) 
-            increment = min(increment, capacity[p[u]][u]);
+    bool dfs(int u, int t) {
+        visited.assign(n, 0);
+        p.assign(n, -1);
+        return __dfs(u, t);
+    }
 
-        for (int u = t; p[u] >= 0; u = p[u]) {
-            capacity[p[u]][u] -= increment;
-            capacity[u][p[u]] += increment;
+public:
+    vector<vector<int>> adj, cap;
+    vector<int> p, visited;
+    int n;
+
+    FordFulkerson(vector<vector<int>> &adj, vector<vector<int>> &cap) : adj(adj), cap(cap) {
+        n = adj.size();
+    }
+
+    int maxflow(int s, int t) {
+        int maxflow = 0;
+        while (dfs(s, t)) {
+            int increment = INF;
+            for (int u = t; p[u] >= 0; u = p[u])
+                increment = min(increment, cap[p[u]][u]);
+
+            for (int u = t; p[u] >= 0; u = p[u]) {
+                cap[p[u]][u] -= increment;
+                cap[u][p[u]] += increment;
+            }
+            maxflow += increment;
         }
-        maxflow += increment;
-        visited.assign(N+1, false);
-        p.assign(N+1, -1);
+        return maxflow;
     }
-    return maxflow;
-}
+};
