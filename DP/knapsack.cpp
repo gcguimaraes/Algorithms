76d4bcf74002b2c360c7f2096b0ff3e03558bdfb
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-11-22 13:35:43 -0300

    Create knapsack.cpp

---
+++ DP/knapsack.cpp
@@ -0,0 +1,9 @@
+int knapsack(int i, int cap, vector<int> &cost, vector<int> &profit, vector<vector<int>> &dp) {
+	if (i < 0) return 0;
+	if (cap < 0) return -INF;
+	if (dp[i][cap] != -1) return dp[i][cap];
+	return dp[i][cap] = max(
+		profit[i] + knapsack(i-1, cap-cost[i], cost, profit, dp),
+		knapsack(i-1, cap, cost, profit, dp)
+	);
+}
