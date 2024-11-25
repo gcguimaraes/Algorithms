15df57e94bb6b8646938bc70b6bee47324395591
gcguimaraes <136760958+gcguimaraes@users.noreply.github.com>
2024-11-25 18:17:17 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const int INF = 0x3F3F3F3F;
 using ii = pair<int, int>;
 using ll = long long;
-#define detachingFromC ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define detachingFromC ios::sync_with_stdio(0);cin.tie(0);
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 
